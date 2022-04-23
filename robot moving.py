b9822f468e8743eff33aff39e6b54ff6853eda4e
omarshafie01 <86484212+omarshafie01@users.noreply.github.com>
2022-04-23 23:12:38 +0200

    Update robot moving.py

--- robot moving.py
+++ robot moving.py
@@ -1,19 +1,19 @@
 from roboticstoolbox import Bicycle, RandomPath, VehicleIcon, RangeBearingSensor,LandmarkMap
-from math import pi, atan2
+import math
 import matplotlib.pyplot as plt
 
 d1 = int(input('please enter the starting x-coordinate:'))
 # user input the x coordinaties
 e1 = int(input('please enter the starting y-coordinate:'))
 # user input the y coordinaties
-f1 = int(input('please enter the starting z-coordinate:'))
 # user input the z coordinaties
+f1 = int(input('please enter the starting z-coordinate:'))
 o1 = int(input('lease enter the number of obstacles:'))
 # user enter number of obstacles
-anim = VehicleIcon('Robot.png', scale = 2)
+animat = VehicleIcon('Robot.png', scale = 2)
 # robot png uploaded
 veh = Bicycle(
- animation=anim,
+ animation=animat,
  control=RandomPath,
  x0=(d1,e1,f1),
 )
@@ -36,3 +36,24 @@ goal_marker_style = {
 # target position, size and colour
 plt.plot(goal[0], goal[1], **goal_marker_style)
 # plot robot and target points
+sensor=RangeBearingSensor(robot=veh, map= map ,animate=True)
+#  rangebearing sensor for sensing near obstacles 
+
+run =True
+while(run):
+ goal_heading = atan2(
+ goal[1] - veh.x[1],
+ goal[0] - veh.x[0]
+ )
+ steer = goal_heading-veh.x[2]
+ veh.step(2,steer)
+ if( (abs(goal[0]-veh.x[0]) > 0.5) or (abs(goal[1]-veh.x[1]) >
+ 0.5) ):
+    run=True
+ else:
+    run =False
+ veh._animation.update(veh.x)
+ plt.pause(0.003)
+#Run robot  for interval seconds
+veh._animation.update(veh.x)
+plt.pause(1000)
